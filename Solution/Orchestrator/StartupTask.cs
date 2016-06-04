2cf7a41cb9d1393a22e358d83e7aa36b2d16d339
Christian Kratky <christian.kratky@googlemail.com>
2016-06-04 22:04:53 +0200

    Add the project

---
+++ Solution/Orchestrator/StartupTask.cs
@@ -0,0 +1,16 @@
+﻿using Windows.ApplicationModel.Background;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace Orchestrator
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // 
+            // TODO: Insert code to start one or more asynchronous methods 
+            //
+        }
+    }
+}
