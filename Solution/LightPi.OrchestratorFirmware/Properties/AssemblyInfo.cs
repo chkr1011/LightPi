48f31b98d673f506148d91fdfe1256077bf95f13
Christian Kratky <christian.kratky@googlemail.com>
2016-07-16 12:08:06 +0200

    Add the project structure

---
+++ Solution/LightPi.OrchestratorFirmware/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LightPi.OrchestratorFirmware")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LightPi")]
+[assembly: AssemblyCopyright("Copyright © Christian Kratky 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ Solution/LightPi.OrchestratorFirmware/UdpEndpoint.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Networking.Sockets;
+using LightPi.Protocol;
+
+namespace LightPi.OrchestratorFirmware
+{
+    internal sealed class UdpEndpoint
+    {
+        private const int Port = 12345;
+        
+        private readonly DatagramSocket _datagramSocket = new DatagramSocket();
+        private readonly Action<byte[]> _callback;
+        
+        public UdpEndpoint(Action<byte[]> callback)
+        {
+            if (callback == null) throw new ArgumentNullException(nameof(callback));
+
+            _callback = callback;
+        }
+
+        public void Open()
+        {
+            // Ensure that the sockets works as fast as any possible to ensure that the animations are running fine.
+            _datagramSocket.Control.DontFragment = true;
+            _datagramSocket.Control.QualityOfService = SocketQualityOfService.LowLatency;
+
+            _datagramSocket.MessageReceived += ProcessIncomingData;
+            _datagramSocket.BindServiceNameAsync(Port.ToString()).AsTask().Wait();
+
+            Debug.WriteLine($"UDP endpoint opened on port {Port}");
+        }
+
+        private void ProcessIncomingData(DatagramSocket sender, DatagramSocketMessageReceivedEventArgs message)
+        {
+            var package = ReadPackageFromMessage(message);
+            if (package == null)
+            {
+                return;
+            }
+
+            _callback(package);
+        }
+
+        private byte[] ReadPackageFromMessage(DatagramSocketMessageReceivedEventArgs message)
+        {
+            using (var reader = message.GetDataReader())
+            {
+                if (reader.UnconsumedBufferLength != LightPiProtocol.PackageLength)
+                {
+                    // Ignore all invalid packages.
+                    Debug.WriteLine($"Received invalid message with a length of {reader.UnconsumedBufferLength} bytes");
+                    return null;
+                }
+
+                var buffer = new byte[LightPiProtocol.PackageLength];
+                reader.ReadBytes(buffer);
+
+                return buffer;
+            }
+        }
+    }
+}
